048102810921f1e3687e4dd6b95c14189ce03db3
renciweb <mwatson@renci.org>
2018-12-14 09:08:11 -0500

    remove console debugging

--- api/controllers/Controller.js
+++ api/controllers/Controller.js
@@ -4,7 +4,6 @@ exports.runQuery = (req, res, query) => {
     db.any(query)
         .then(data => {
             console.log(`HIT: ${req.path}`)
-            console.log(data)
             res.status(200).send(data)
         })
         .catch(error => {
