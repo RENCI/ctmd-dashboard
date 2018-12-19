385f6f03cd096f72162a86185483d1052920a888
renciweb <mwatson@renci.org>
2018-12-19 11:48:53 -0500

    api root path fix

--- frontend/src/views/Dashboard/Proposals/ByApproval.js
+++ frontend/src/views/Dashboard/Proposals/ByApproval.js
@@ -5,7 +5,9 @@ import Heading from '../../../components/Typography/Heading'
 import ApprovedProposals from '../../../components/Dashboard/Proposals/Approved'
 
 const proposalsByApproval = (props) => {
-    const apiRoot = process.env.NODE_END === 'production' ? 'https://pmd.renci.org/api' : 'http://localhost:3030'
+
+    const apiRoot = process.env.NODE_ENV === 'production' ? 'https://pmd.renci.org/api' : 'http://localhost:3030'
+
     return (
         <div>
             <Heading>Proposals by Approval</Heading>
--- frontend/src/views/Dashboard/Proposals/BySubmission.js
+++ frontend/src/views/Dashboard/Proposals/BySubmission.js
@@ -5,7 +5,9 @@ import Heading from '../../../components/Typography/Heading'
 import SubmittedProposals from '../../../components/Dashboard/Proposals/Submitted'
 
 const proposalsBySubmission = (props) => {
-    const apiRoot = process.env.NODE_END === 'production' ? 'https://pmd.renci.org/api' : 'http://localhost:3030'
+    
+    const apiRoot = process.env.NODE_ENV === 'production' ? 'https://pmd.renci.org/api' : 'http://localhost:3030'
+    
     return (
         <div>
             <Heading>Proposals by Approval</Heading>
