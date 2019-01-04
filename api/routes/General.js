6ef364236c1b0914c762f344b7cdc2ba55c45997
renciweb <mwatson@renci.org>
2019-01-03 23:00:19 -0500

    add route to grab name/description pairs

--- api/routes/General.js
+++ api/routes/General.js
@@ -4,8 +4,19 @@ const controller = require('../controllers/QueryController')
 
 // PIs
 router.get('/pis', (req, res) => {
-    query = `SELECT DISTINCT *
-        FROM "PI";`
+    query = `SELECT DISTINCT * FROM "PI";`
+    controller.runQuery(req, res, query)
+})
+
+// Name & dscription, given name -- from dropdowns
+router.get('/name/:id', (req, res) => {
+    query = `SELECT * FROM name WHERE id='${req.params.id}';`
+    controller.runQuery(req, res, query)
+})
+
+// Names & dscriptions, given name -- from dropdowns
+router.get('/name/like/:id', (req, res) => {
+    query = `SELECT * FROM name WHERE id like '${req.params.id}%';`
     controller.runQuery(req, res, query)
 })
 
