801a3238d20a40a598fcd609be154d03861f2443
renciweb <mwatson@renci.org>
2019-01-10 13:44:07 -0500

    remove unused vars

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,5 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
 import * as serviceWorker from './serviceWorker'
 import { makeMainRoutes } from './Routes'
 import WebFont from 'webfontloader'
