0b01e4c25e1a02b5d906ad883255dc1a516f9d50
renciweb <mwatson@renci.org>
2019-01-19 10:30:39 -0500

    extract helper functions to utils directory

--- api/controllers/Proposals.js
+++ api/controllers/Proposals.js
@@ -1,20 +1,5 @@
 const db = require('../config/database')
 
-// Helper Functions
-///////////////////
-
-// Parse array-like string returned by Postgres into a real array
-const stringToArray = (str) => str.slice(1, -1).split(',').filter(el => el != '')
-// Helper function to sort proposals by proposal_id
-const compareIds = (p, q) => (p.proposal_id < q.proposal_id) ? -1 : 1
-// Convert string to CamelCase
-const camelCase = str => {
-    let string = str.toLowerCase().replace(/[^A-Za-z0-9]/g, ' ').split(' ')
-        .reduce((result, word) => result + capitalize(word.toLowerCase()))
-    return string.charAt(0).toLowerCase() + string.slice(1)
-}
-const capitalize = str => str.charAt(0).toUpperCase() + str.toLowerCase().slice(1)
-
 // Controllers
 //////////////
 
