701c3e74a2bce4d3a10569eb562b33318e6a767f
renciweb <mwatson@renci.org>
2019-01-24 15:40:53 -0500

    add stacked bar chart

--- frontend/src/components/Charts/ProposalsByTic.js
+++ frontend/src/components/Charts/ProposalsByTic.js
@@ -1,25 +1,6 @@
 import React from 'react'
 import { ResponsiveBar } from '@nivo/bar'
 
-const data = [
-    { "TIC": "AD", "hot dog": 191, "hot dogColor": "hsl(264, 70%, 50%)", "burger": 193, "burgerColor": "hsl(150, 70%, 50%)", "sandwich": 164, "sandwichColor": "hsl(129, 70%, 50%)", "kebab": 122, "kebabColor": "hsl(261, 70%, 50%)", "fries": 183, "friesColor": "hsl(125, 70%, 50%)", "donut": 176, "donutColor": "hsl(211, 70%, 50%)" },
-    { "TIC": "AE", "hot dog": 53, "hot dogColor": "hsl(240, 70%, 50%)", "burger": 94, "burgerColor": "hsl(267, 70%, 50%)", "sandwich": 39, "sandwichColor": "hsl(121, 70%, 50%)", "kebab": 149, "kebabColor": "hsl(94, 70%, 50%)", "fries": 73, "friesColor": "hsl(173, 70%, 50%)", "donut": 17, "donutColor": "hsl(294, 70%, 50%)" },
-    { "TIC": "AF", "hot dog": 117, "hot dogColor": "hsl(59, 70%, 50%)", "burger": 176, "burgerColor": "hsl(135, 70%, 50%)", "sandwich": 16, "sandwichColor": "hsl(196, 70%, 50%)", "kebab": 124, "kebabColor": "hsl(90, 70%, 50%)", "fries": 97, "friesColor": "hsl(148, 70%, 50%)", "donut": 35, "donutColor": "hsl(58, 70%, 50%)" },
-    { "TIC": "AG", "hot dog": 88, "hot dogColor": "hsl(313, 70%, 50%)", "burger": 167, "burgerColor": "hsl(140, 70%, 50%)", "sandwich": 34, "sandwichColor": "hsl(48, 70%, 50%)", "kebab": 77, "kebabColor": "hsl(34, 70%, 50%)", "fries": 181, "friesColor": "hsl(205, 70%, 50%)", "donut": 129, "donutColor": "hsl(283, 70%, 50%)" },
-    { "TIC": "AI", "hot dog": 100, "hot dogColor": "hsl(173, 70%, 50%)", "burger": 36, "burgerColor": "hsl(80, 70%, 50%)", "sandwich": 180, "sandwichColor": "hsl(231, 70%, 50%)", "kebab": 34, "kebabColor": "hsl(244, 70%, 50%)", "fries": 154, "friesColor": "hsl(117, 70%, 50%)", "donut": 102, "donutColor": "hsl(349, 70%, 50%)" },
-    { "TIC": "AL", "hot dog": 175, "hot dogColor": "hsl(239, 70%, 50%)", "burger": 36, "burgerColor": "hsl(332, 70%, 50%)", "sandwich": 60, "sandwichColor": "hsl(293, 70%, 50%)", "kebab": 118, "kebabColor": "hsl(225, 70%, 50%)", "fries": 65, "friesColor": "hsl(139, 70%, 50%)", "donut": 15, "donutColor": "hsl(29, 70%, 50%)" },
-    { "TIC": "AM", "hot dog": 95, "hot dogColor": "hsl(141, 70%, 50%)", "burger": 46, "burgerColor": "hsl(346, 70%, 50%)", "sandwich": 166, "sandwichColor": "hsl(255, 70%, 50%)", "kebab": 140, "kebabColor": "hsl(212, 70%, 50%)", "fries": 191, "friesColor": "hsl(124, 70%, 50%)", "donut": 77, "donutColor": "hsl(89, 70%, 50%)" }
-]
-
-Array.prototype.groupBy = function(prop) {
-    return this.reduce(function(groups, item) {
-        const val = item[prop]
-        groups[val] = groups[val] || []
-        groups[val].push(item)
-        return groups
-    }, {})
-}
-
 Array.prototype.countBy = function(prop) {
     return this.reduce(function(groups, item) {
         const val = item[prop]
@@ -29,6 +10,7 @@ Array.prototype.countBy = function(prop) {
     }, {})
 }
 
+// * Create endpoint to pull these from the database
 const statuses = [
     'Submitted',
     'Pending Submission',
