2023e01a2d339e877b1a2a15a8017af249d6995e
renciweb <mwatson@renci.org>
2019-01-26 16:12:50 -0500

    center spinner

--- frontend/src/components/Spinner/Spinner.js
+++ frontend/src/components/Spinner/Spinner.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { CircularProgress } from '@material-ui/core'
 
 function CircularUnderLoad() {
-    return <CircularProgress style={{ textAlign: 'center' }} disableShrink />
+    return <CircularProgress style={{ display: 'block', margin: 'auto' }} disableShrink />
 }
 
 export default CircularUnderLoad
