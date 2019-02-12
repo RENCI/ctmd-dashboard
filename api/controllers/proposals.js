56a954c6ce8d58deccace8e7af292ad7611d126a
mbwatson <mattwat@gmail.com>
2019-02-12 16:06:36 -0500

    add proposal dates to proposals list response

--- api/controllers/proposals.js
+++ api/controllers/proposals.js
@@ -22,6 +22,7 @@ exports.list = (req, res) => {
     query = `SELECT DISTINCT
             CAST(proposal.proposal_id AS INT),
             proposal.short_name,
+            proposal.prop_submit,
             TRIM(CONCAT(proposal.pi_firstname, ' ', proposal.pi_lastname)) AS pi_name,
             name.description AS proposal_status,
             name2.description AS tic_name,
@@ -39,6 +40,9 @@ exports.list = (req, res) => {
         ORDER BY proposal_id;`
     db.any(query)
         .then(data => {
+            data.forEach(proposal => {
+                proposal.submission_date = proposal.prop_submit.toDateString()
+            })
             res.status(200).send(data)
         })
         .catch(error => {
