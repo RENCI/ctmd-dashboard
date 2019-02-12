3f0758806dff2c0e601aa428db8aa46c9652cd41
David Borland <borland@renci.org>
2019-02-12 13:51:20 -0500

    Convert ProposalsNetworkContainer to functional component with hooks

--- frontend/src/components/Visualizations/ProposalsNetworkContainer.js
+++ frontend/src/components/Visualizations/ProposalsNetworkContainer.js
@@ -1,62 +1,50 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import axios from 'axios';
 import Controls from './ProposalsNetworkControls';
 import Visualizations from './ProposalsNetworkVisualizations';
 
-class ProposalsNetworkContainer extends Component {
-    constructor(props) {
-        super(props);
+function  ProposalsNetworkContainer(props) {
+    const [proposals, setProposals] = useState([]);
+    const [selectedProposals, setSelectedProposals] = useState([]);
 
-        this.state = {
-            proposals: [],
-            selectedProposals: []
-        };
-
-        this.handleControlChange = this.handleControlChange.bind(this);
-    }
-
-    handleControlChange(name, event) {
-        switch (name) {
-            case 'status':
-                let status = event.target.value;
-
-                this.setState({
-                      selectedProposals: this.state.proposals.filter(proposal =>
-                          proposal.proposal_status === status
-                      ).map(proposal => proposal.proposal_id)
-                });
-
-                break;
-            default:
-        }
-    }
-
-    async fetchData() {
-        await axios.get(this.props.apiUrl)
+    async function fetchData() {
+        await axios.get(props.apiUrl)
             .then(response => {
-                this.setState({
-                    proposals: response.data
-                });
+                setProposals(response.data);
             })
             .catch(error => {
                 console.error('Error:', error)
             });
     }
 
-    componentWillMount = this.fetchData;
+    useEffect(() => {
+      fetchData();
+    }, []);
 
-    render() {
-        return (
-            <Fragment>
-                <Controls
-                    proposals={ this.state.proposals }
-                    onChange={ this.handleControlChange } />
-                <Visualizations
-                    proposals={ this.state.proposals }
-                    selectedProposals={ this.state.selectedProposals } />
-            </Fragment>
-        );
+    function handleControlChange(name, event) {
+        switch (name) {
+            case 'status':
+                let status = event.target.value;
+
+                setSelectedProposals(proposals.filter(proposal =>
+                    proposal.proposal_status === status
+                ).map(proposal => proposal.proposal_id));
+
+                break;
+            default:
+        }
     }
+
+    return (
+        <Fragment>
+            <Controls
+                proposals={ proposals }
+                onChange={ handleControlChange } />
+            <Visualizations
+                proposals={ proposals }
+                selectedProposals={ selectedProposals } />
+        </Fragment>
+    );
 }
 
 export default ProposalsNetworkContainer
