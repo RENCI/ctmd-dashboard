afb59455a4c01876239f34ca791b38cc28acbf5b
mbwatson <mattwat@gmail.com>
2019-02-21 13:25:13 -0500

    add expansion detail panel to proposal table

--- frontend/src/components/Charts/ProposalsTable.js
+++ frontend/src/components/Charts/ProposalsTable.js
@@ -1,5 +1,97 @@
 import React from 'react'
+import classnames from 'classnames'
+import { makeStyles } from '@material-ui/styles'
 import MaterialTable from 'material-table'
+import { Grid, List, ListItemIcon, ListItem, ListItemText } from '@material-ui/core'
+import Subheading from '../Typography/Subheading'
+import {
+    AccountBox as PiIcon,
+    CalendarToday as CalendarIcon,
+    AccountBalance as InstitutionIcon,
+    LocalOffer as TherapeuticAreaIcon,
+    Assignment as TicIcon,
+    Alarm as ProposalStatusIcon,
+    AttachMoney as BudgetIcon,
+    Timelapse as FundingDurationIcon,
+} from '@material-ui/icons'
+
+const useStyles = makeStyles(theme => ({
+    panel: {
+        // ...theme.mixins.debug,
+        padding: `${ 2 * theme.spacing.unit }px ${ 4 * theme.spacing.unit }px`,
+        backgroundColor: theme.palette.common.white,
+    },
+    panelHeader: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingBottom: 2 * theme.spacing.unit,
+        marginBottom: 2 * theme.spacing.unit,
+        borderBottom: `1px solid ${ theme.palette.grey[300] }`,
+    },
+    panelVisualizationColumn: {
+        borderLeft: `1px solid ${ theme.palette.grey[300] }`,
+        padding: 2 * theme.spacing.unit,
+    },
+}))
+
+const ProposalDetailPanel = props => {
+    const { proposal_id, short_name, pi_name, org_name, tic_name, submission_date, therapeutic_area, proposal_status, anticipated_budget, funding_duration } = props
+    const classes = useStyles()
+    return (
+        <Grid container className={ classes.panel }>
+            <Grid item xs={ 12 } className={ classes.panelHeader }>
+                <Subheading>{ short_name }</Subheading>
+                <span>#{ proposal_id }</span>
+            </Grid>
+            <Grid item xs={ 2 }>
+                <List dense>
+                    <ListItem>
+                        <ListItemIcon><PiIcon /></ListItemIcon>
+                        <ListItemText primary={ pi_name } />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemIcon><CalendarIcon /></ListItemIcon>
+                        <ListItemText primary={ submission_date } />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemIcon><TherapeuticAreaIcon /></ListItemIcon>
+                        <ListItemText primary={ therapeutic_area } />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemIcon><InstitutionIcon /></ListItemIcon>
+                        <ListItemText primary={ org_name } />
+                    </ListItem>
+                </List>
+            </Grid>
+            <Grid item xs={ 2 }>
+                <List dense>
+                    <ListItem>
+                        <ListItemIcon><TicIcon /></ListItemIcon>
+                        <ListItemText primary={ tic_name } />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemIcon><ProposalStatusIcon /></ListItemIcon>
+                        <ListItemText primary={ proposal_status } />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemIcon><BudgetIcon /></ListItemIcon>
+                        <ListItemText primary={ anticipated_budget } />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemIcon><FundingDurationIcon /></ListItemIcon>
+                        <ListItemText primary={ funding_duration } />
+                    </ListItem>
+                </List>
+            </Grid>
+            <Grid item xs={ 6 } className={ classes.panelVisualizationColumn }>
+                <div>
+                    Some visualization. Perhaps days elapsed between submission, approval, etc.
+                </div>
+            </Grid>
+        </Grid>
+    )
+}
 
 const ProposalsTable = (props) => {
     const { proposals } = props
@@ -7,7 +99,6 @@ const ProposalsTable = (props) => {
         <MaterialTable
             components={{ }}
             columns={ [
-                { title: 'Proposal ID', field: 'proposal_id', },
                 { title: 'Proposal Name', field: 'short_name', },
                 { title: 'PI', field: 'pi_name', },
                 { title: 'Proposal Status', field: 'proposal_status', },
@@ -27,6 +118,8 @@ const ProposalsTable = (props) => {
                 pageSizeOptions: [15, 25, 50],
             }}
             title=""
+            detailPanel={rowData => <ProposalDetailPanel { ...rowData } />}
+            onRowClick={(event, rowData, togglePanel) => togglePanel()}
         />
     )
 }
