fa3681669eabe658f196cbc9cfda2cb7ee149808
mbwatson <mattwat@gmail.com>
2019-03-15 09:34:16 -0400

    add icons to proposal-browsing menu

--- frontend/src/components/Menus/BrowseMenu.js
+++ frontend/src/components/Menus/BrowseMenu.js
@@ -1,8 +1,17 @@
 import React, { Fragment, useState } from 'react'
 import { useTheme } from '@material-ui/styles'
-import { Button, Menu, ListItemText, MenuItem, Divider } from '@material-ui/core'
+import { Button, Menu, MenuItem, ListItemText, ListItemIcon, Divider } from '@material-ui/core'
 import { NavLink } from 'react-router-dom'
 import { KeyboardArrowDown as SubmenuIcon } from '@material-ui/icons'
+import {
+    List as ListIcon,
+    CalendarToday as CalendarIcon,
+    AccountBalance as InstitutionIcon,
+    LocalOffer as TherapeuticAreaIcon,
+    Assignment as TicIcon,
+    Alarm as StatusIcon,
+    LocalLaundryService as ServicesIcon,
+} from '@material-ui/icons'
 
 const BrowseMenu = (props) => {
     const [anchorEl, setAnchorEl] = useState(null)
@@ -33,32 +42,40 @@ const BrowseMenu = (props) => {
                 onClose={ handleClose }
             >
                 <MenuItem component={ NavLink } exact to="/proposals">
+                    <ListItemIcon><ListIcon /></ListItemIcon>
                     <ListItemText primary="List All"/>
                 </MenuItem>
                 <Divider />
                 <MenuItem component={ NavLink } to="/proposals/date">
+                    <ListItemIcon><CalendarIcon /></ListItemIcon>
                     <ListItemText primary="Date"/>
                 </MenuItem>
                 <Divider />
                 <MenuItem component={ NavLink } to="/proposals/organization">
+                    <ListItemIcon><InstitutionIcon /></ListItemIcon>
                     <ListItemText primary="Submitting Institution"/>
                 </MenuItem>
                 <MenuItem component={ NavLink } to="/proposals/tic">
+                    <ListItemIcon><TicIcon /></ListItemIcon>
                     <ListItemText primary="Assigned TIC/RIC"/>
                 </MenuItem>
                 <Divider />
                 <MenuItem component={ NavLink } to="/proposals/therapeutic-area">
+                    <ListItemIcon><TherapeuticAreaIcon /></ListItemIcon>
                     <ListItemText primary="Therapeutic Area"/>
                 </MenuItem>
                 <Divider />
                 <MenuItem component={ NavLink } to="/proposals/requested-services">
+                    <ListItemIcon><ServicesIcon /></ListItemIcon>
                     <ListItemText primary="Services Requested"/>
                 </MenuItem>
                 <MenuItem component={ NavLink } to="/proposals/approved-services">
+                    <ListItemIcon><ServicesIcon /></ListItemIcon>
                     <ListItemText primary="Services Approved"/>
                 </MenuItem>
                 <Divider />
                 <MenuItem component={ NavLink } to="/proposals/status">
+                    <ListItemIcon><StatusIcon /></ListItemIcon>
                     <ListItemText primary="Status"/>
                 </MenuItem>
             </Menu>
