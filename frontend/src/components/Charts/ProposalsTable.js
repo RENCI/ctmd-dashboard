c4e186b58077d0c403e727f5d109afd5b518cd1f
mbwatson <mattwat@gmail.com>
2019-03-27 14:34:52 -0400

    add funding-status-when-approved to proposal table expansion panels

--- frontend/src/components/Charts/ProposalsTable.js
+++ frontend/src/components/Charts/ProposalsTable.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useContext } from 'react'
 import { makeStyles } from '@material-ui/styles'
 import MaterialTable from 'material-table'
-import { Grid, Typography, List, Tooltip, ListItemIcon, ListItem, ListItemText } from '@material-ui/core'
+import { Grid, Typography, List, Tooltip, ListItemIcon, ListItem, ListItemText, Chip } from '@material-ui/core'
 import {
     AccountBox as PiIcon,
     CalendarToday as CalendarIcon,
@@ -11,13 +11,14 @@ import {
     Alarm as ProposalStatusIcon,
     AttachMoney as BudgetIcon,
     LocalLaundryService as ServicesIcon,
+    CheckCircle as ApprovedIcon,
 } from '@material-ui/icons'
 import { SettingsContext } from '../../contexts/SettingsContext'
 
 const useStyles = makeStyles(theme => ({
     panel: {
         padding: `${ 2 * theme.spacing.unit }px ${ 4 * theme.spacing.unit }px`,
-        backgroundColor: theme.palette.common.white,
+        backgroundColor: theme.palette.extended.gingerBeer,
     },
     header: {
         display: 'flex',
@@ -32,7 +33,10 @@ const useStyles = makeStyles(theme => ({
         fontWeight: 'bold',
         letterSpacing: '1px',
     },
-    proposalId: { },
+    chip: {
+        margin: theme.spacing.unit,
+        fontWeight: 'bold',
+    },
     column1: {
         borderRight: `1px solid ${ theme.palette.grey[300] }`,
     },
@@ -61,11 +65,13 @@ const useStyles = makeStyles(theme => ({
 const ProposalDetailPanel = props => {
     const {
         proposalID, shortTitle, piName, submitterInstitution, assignToInstitution,
-        therapeuticArea, proposalStatus, totalBudget, fundingPeriod,
+        therapeuticArea, proposalStatus, totalBudget, fundingPeriod, fundingStatus, fundingStatusWhenApproved,
         dateSubmitted, meetingDate, fundingStart, plannedGrantSubmissionDate,
         requestedServices, approvedServices,
     } = props
     const classes = useStyles()
+
+    console.log(fundingStatusWhenApproved)
     
     const todaysDate = (new Date).toISOString().slice(0,10)
 
@@ -75,42 +81,50 @@ const ProposalDetailPanel = props => {
         <Grid container className={ classes.panel }>
             <Grid item xs={ 12 } className={ classes.header }>
                 <Typography variant="h5" className={ classes.title }>{ shortTitle }</Typography>
-                <span className={ classes.proposalId }>#{ proposalID }</span>
+                <Chip label={ `#${ proposalID }` } className={ classes.chip } color="secondary"/>
             </Grid>
-            <Grid item xs={ 3 } className={ classes.column1 }>
+            <Grid item xs={ 4 } className={ classes.column1 }>
                 <List dense>
                     <ListItem>
                         <Tooltip title="PI" aria-label="PI"><ListItemIcon><PiIcon /></ListItemIcon></Tooltip>
-                        <ListItemText primary={ piName } />
+                        <ListItemText primary={ piName || '-' } />
                     </ListItem>
                     <ListItem>
                         <Tooltip title="Submitting Institution" aria-label="Submitting Institution"><ListItemIcon><InstitutionIcon /></ListItemIcon></Tooltip>
-                        <ListItemText primary={ submitterInstitution } />
+                        <ListItemText primary={ submitterInstitution || '-' } />
                     </ListItem>
                     <ListItem>
                         <Tooltip title="Assigned TIC/RIC" aria-label="Assigned TIC/RIC"><ListItemIcon><TicIcon /></ListItemIcon></Tooltip>
-                        <ListItemText primary={ assignToInstitution } />
+                        <ListItemText primary={ assignToInstitution || '-' } />
                     </ListItem>
-                </List>
-            </Grid>
-            <Grid item xs={ 3 } className={ classes.column2 }>
-                <List dense>
                     <ListItem>
                         <Tooltip title="Therapeutic Area" aria-label="Therapeutic Area"><ListItemIcon><TherapeuticAreaIcon /></ListItemIcon></Tooltip>
-                        <ListItemText primary={ therapeuticArea } />
+                        <ListItemText primary={ therapeuticArea || '-' } />
                     </ListItem>
                     <ListItem>
                         <Tooltip title="Proposal Status" aria-label="Proposal Status"><ListItemIcon><ProposalStatusIcon /></ListItemIcon></Tooltip>
-                        <ListItemText primary={ proposalStatus } />
-                    </ListItem>
-                    <ListItem>
-                        <Tooltip title="Budget" aria-label="Budget"><ListItemIcon><BudgetIcon /></ListItemIcon></Tooltip>
-                        <ListItemText primary={ totalBudget } />
-                        <ListItemText primary={ fundingPeriod } />
+                        <ListItemText primary={ proposalStatus || '-' } />
                     </ListItem>
                 </List>
             </Grid>
-            <Grid item xs={ 6 }>
+            <Grid item xs={ 4 } className={ classes.column2 }>
+                <List dense>
+                    <ListItem>
+                        <Tooltip title="Funding" aria-label="Funding"><ListItemIcon><BudgetIcon /></ListItemIcon></Tooltip>
+                        <ListItemText primary={ fundingStatus || '-' } secondary={ `${ fundingPeriod || '-' } / ${ totalBudget || '-' }` } />
+                    </ListItem>
+                    {
+                        fundingStatusWhenApproved ? (
+                            <ListItem>
+                                <Tooltip title="Funding" aria-label="Funding"><ListItemIcon><ApprovedIcon /></ListItemIcon></Tooltip>
+                                <ListItemText primary="Approval Funding Status" secondary={ fundingStatusWhenApproved || '-' } />
+                            </ListItem>
+
+                        ) : null
+                    }
+                </List>
+            </Grid>
+            <Grid item xs={ 4 }>
                 <List dense>
                     <ListItem className={ classes.servicesRow }>
                         <Tooltip title="Services" aria-label="Requested and Approved Services"><ListItemIcon><ServicesIcon /></ListItemIcon></Tooltip>
@@ -188,7 +202,7 @@ const ProposalsTable = (props) => {
     if (title) title += ` (${ proposals.length } Proposals)`
     return (
         <MaterialTable
-            title={ title || '' }
+            title={ title || '-' }
             components={{ }}
             columns={ [
                 { title: 'ID', field: 'proposalID', hidden: false },
@@ -213,6 +227,7 @@ const ProposalsTable = (props) => {
                 pageSize: 15,
                 pageSizeOptions: [15, 25, 50, 100, 200],
                 exportFileName: title,
+                detailPanelType: "single",
             }}
             detailPanel={rowData => <ProposalDetailPanel { ...rowData } />}
             onRowClick={(event, rowData, togglePanel) => togglePanel()}
