94b3964e3618f6ea17b7115591b88592dd3a8734
mbwatson <mattwat@gmail.com>
2019-03-27 14:16:39 -0400

    add in fundingStatus and fundingStatusWhenApproved to proposals query

--- api/controllers/proposals.js
+++ api/controllers/proposals.js
@@ -26,6 +26,8 @@ const proposalsQuery = `SELECT CAST("Proposal"."ProposalID" AS INT) as "proposal
             name2.description AS "assignToInstitution",
             name3.description AS "submitterInstitution",
             name4.description AS "therapeuticArea",
+            name5.description AS "fundingStatus",
+            name6.description AS "fundingStatusWhenApproved",
             "ProposalFunding"."totalBudget",
             CAST("ProposalFunding"."fundingPeriod" AS VARCHAR),
             CAST("ProposalFunding"."fundingStart" AS VARCHAR),
@@ -42,6 +44,8 @@ const proposalsQuery = `SELECT CAST("Proposal"."ProposalID" AS INT) as "proposal
         LEFT JOIN name name2 ON name2.index = "AssignProposal"."assignToInstitution" AND name2."column" = 'assignToInstitution'
         INNER JOIN name name3 ON name3.index = "Submitter"."submitterInstitution" AND name3."column" = 'submitterInstitution'
         INNER JOIN name name4 ON name4.index = "ProposalDetails"."therapeuticArea" AND name4."column" = 'therapeuticArea'
+        INNER JOIN name name5 ON name5.index = "ProposalFunding"."currentFunding" AND name5."column" = 'currentFunding'
+        LEFT JOIN name name6 ON name6.index = "ProposalFunding"."newFundingStatus" AND name6."column" = 'newFundingStatus'
         ORDER BY "proposalID";`
 
 const query2 = `SELECT "Proposal"."ProposalID" as "proposalID",                  
