e543f6b57020d9806282ec57f1149fa579c1ef03
mbwatson <mattwat@gmail.com>
2019-04-03 10:35:01 -0400

    style flash message and container
    use single flashMessage file with named exports

--- frontend/src/components/FlashMessage/FlashMessage.js
+++ frontend/src/components/FlashMessage/FlashMessage.js
@@ -4,13 +4,34 @@ import { Close as CloseIcon } from '@material-ui/icons'
 import { Snackbar, IconButton, Button } from '@material-ui/core'
 
 const useStyles = makeStyles(theme => ({
-    snackbar: {
+    flashMessageContainer: {
+        position: 'fixed',
+        left: '50%',
+        right: 'auto',
+        bottom: 0,
+        transform: 'translateX(-50%)',
+        display: 'flex',
+        flexDirection: 'column-reverse',
+    },
+    flashMessage: {
         position: 'relative',
-        marginBottom: 2 * theme.spacing.unit,
+        marginBottom: theme.spacing.unit,
     },
 }))
 
-const FlashMessage = props => {
+
+export const FlashMessageContainer = props => {
+    const { children } = props
+    const classes = useStyles()
+
+    return (
+        <div className={ classes.flashMessageContainer }>
+            { children }
+        </div>
+    )
+}
+
+export const FlashMessage = props => {
     const [open, setOpen] = useState(true)
     const { message } = props
     const classes = useStyles()
@@ -30,7 +51,7 @@ const FlashMessage = props => {
 
     return (
         <Snackbar
-            className={ classes.snackbar }
+            className={ classes.flashMessage }
             anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
             open={ open }
             autoHideDuration={ 3000 }
@@ -45,5 +66,3 @@ const FlashMessage = props => {
         />
     )
 }
-
-export default FlashMessage
