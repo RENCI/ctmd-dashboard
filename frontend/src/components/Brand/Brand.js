55880c4617031109800fc6dba24c32a93afcb114
Matt Watson <mattwat@gmail.com>
2019-05-17 13:16:07 -0400

    add branding

---
+++ frontend/src/components/Brand/Brand.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import classnames from 'classnames'
+import { makeStyles } from '@material-ui/styles'
+
+const useStyles = makeStyles(theme => ({
+    brand: {
+        color: theme.palette.primary.light,
+        fontFamily: 'EB Garamond',
+        fontSize: '200%',
+        backgroundColor: 'transparent',
+        padding: `${2 * theme.spacing.unit }px`,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+        opacity: 0.33,
+        writingMode: 'vertical-rl',
+        transform: 'rotate(180deg)',
+        pointerEvents: 'none',
+    },
+}))
+
+const Brand = props => {
+    const classes = useStyles()
+    
+    return (
+        <div className={ classes.brand }>
+            Duke/Vanderbilt&nbsp;TIC
+        </div>
+    )
+}
+
+export default Brand
