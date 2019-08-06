f8af1f6908070352895d1263a7669d6def72be84
Matt Watson <mattwat@gmail.com>
2019-08-06 17:14:10 -0400

    restyle ctsa icon

--- frontend/src/components/Icons/Ctsa.js
+++ frontend/src/components/Icons/Ctsa.js
@@ -3,10 +3,10 @@ import createSvgIcon from '@material-ui/icons/utils/createSvgIcon'
 
 export const CTSAIcon = createSvgIcon(
     <Fragment>
-        <text fontSize="16" fontWeight="bold" x="2" y="12">C</text>
-        <text fontSize="16" fontWeight="bold" x="14" y="12">T</text>
-        <text fontSize="16" fontWeight="bold" x="2" y="24">S</text>
-        <text fontSize="16" fontWeight="bold" x="14" y="24">A</text>
+        <text fontSize="14" fontWeight="bold" x="2" y="11">C</text>
+        <text fontSize="14" fontWeight="bold" x="13" y="11">T</text>
+        <text fontSize="14" fontWeight="bold" x="2" y="24">S</text>
+        <text fontSize="14" fontWeight="bold" x="13" y="24">A</text>
     </Fragment>
     , 'CTSAIcon'
 )
