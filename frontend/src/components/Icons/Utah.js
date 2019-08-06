adf6e59c169e431c00d7aa0eba9da89b9bc6913b
Matt Watson <mattwat@gmail.com>
2019-08-06 17:09:45 -0400

    remove utah icon

--- frontend/src/components/Icons/Utah.js
+++
@@ -1,9 +0,0 @@
-import React, { Fragment } from 'react'
-import createSvgIcon from '@material-ui/icons/utils/createSvgIcon'
-
-export const UtahIcon = createSvgIcon(
-    <Fragment>
-        <path d="m11.68 4.86v-4.86h-11.68v24h18.464v-19.14zm-2.692 9.34c-0.016 0.952-0.332 1.72-0.936 2.284-0.604 0.576-1.376 0.872-2.296 0.88-0.908-8e-3 -1.672-0.304-2.28-0.88-0.624-0.56-0.944-1.328-0.952-2.28v-5.844h1.7v5.7c8e-3 0.524 0.152 0.912 0.436 1.176l4e-3 4e-3c0.284 0.284 0.64 0.42 1.088 0.42 0.452 0 0.812-0.136 1.104-0.42 0.284-0.268 0.424-0.656 0.428-1.184v-5.696h1.7v5.84zm6.956-4.28h-2.332v7.372h-1.704v-7.372h-2.332v-1.56h6.368z" strokeWidth=".04"/>
-    </Fragment>
-    , 'UtahIcon'
-)
