c29a0729216448b5ab5369004f021cb0170fcc80
Matt Watson <mattwat@gmail.com>
2019-08-06 16:38:08 -0400

    display sites and ctsas with lookup table components

--- frontend/src/views/Ctsas.js
+++ frontend/src/views/Ctsas.js
@@ -1,20 +1,29 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import api from '../Api'
 import { Title } from '../components/Typography'
 import { Grid } from '@material-ui/core'
+import { CircularLoader } from '../components/Progress/Progress'
+import { LookupTable } from '../components/Tables/LookupTable'
 
 export const CtsasPage = (props) => {
+    const [ctsas, setCtsas] = useState(null)
+
+    useEffect(() => {
+        const fetchCtsas = async () => {
+            await axios.get(api.ctsas)
+                .then(response => setCtsas(response.data))
+                .catch(error => console.error(error))
+        }
+        fetchCtsas()
+    }, [])
+
     return (
         <div>
             <Title>CTSAs</Title>
 
-            <Grid container>
+            { ctsas ? <LookupTable title="CTSAs" data={ ctsas } /> : <CircularLoader /> }
             
-              <Grid item xs={ 12 }>
-                  Lorem ipsum dolor sit.
-              </Grid>
-            
-            </Grid>
         </div>
     )
 }
