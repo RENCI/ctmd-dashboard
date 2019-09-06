a8b19ffe6c90bdce9270248c2f796f78b6a0d870
Matt Watson <mattwat@gmail.com>
2019-09-06 12:53:06 -0400

    remove milestones temporarily

--- frontend/src/views/Studies/Report.js
+++ frontend/src/views/Studies/Report.js
@@ -13,111 +13,6 @@ import { SitesActivationPieChart } from '../../components/Charts'
 import StudyEnrollment from '../../components/Visualizations/StudyEnrollmentContainer'
 import { formatDate } from '../../utils'
 
-Array.prototype.chunk = function(size) {
-    var chunks = []
-    for (var i = 0; i < this.length; i += size) {
-        chunks.push(this.slice(i,i + size))
-    }
-    return chunks
-}
-
-const Milestones = ({ sites }) => {
-    const [patientCounts, setPatientCounts] = useState({ consented: 0, enrolled: 0, withdrawn: 0, expected: 0})
-    const [firstIRBApprovedDate, setFirstIRBApprovedDate] = useState()
-    const [firstSiteActivationDate, setFirstSiteActivationDate] = useState()
-    const [firstSubjectEnrolled, setFirstSubjectEnrolled] = useState()
-    const [siteActivationPercentages, setSiteActivationPercentages] = useState([null, null, null, null])
-
-    const earliestDate = property => {
-        const reducer = (earliestDate, date) => date < new Date(earliestDate) ? date : earliestDate
-        const earliestDate = sites.map(site => site[property])
-            .filter(date => date !== '')
-            .map(date => new Date(date))
-            .reduce(reducer, new Date())
-        return formatDate(earliestDate)
-    }
-
-    const thresholds = property => {
-        const quartileSize = Math.round(sites.length / 4)
-        const activationDates = sites.map(site => site[property])
-            .filter(date => date !== '')
-            .map(date => new Date(date))
-            .sort((d1, d2) => d1 > d2)
-        const dates = activationDates.chunk(quartileSize).map(chunk => formatDate(chunk[chunk.length - 1]))
-        for (let i = 0; i < 4; i++) {
-            if (!dates[i]) { dates[i] = 'N/A' }
-        }
-        setSiteActivationPercentages(dates)
-    }
-
-    useEffect(() => {
-        setFirstIRBApprovedDate(earliestDate('dateIrbApproval'))
-        setFirstSiteActivationDate(earliestDate('dateSiteActivated'))
-        setFirstSubjectEnrolled(earliestDate('fpfv'))
-        thresholds('dateSiteActivated')
-    }, [sites])
-
-    const activeSitesCount = () => {
-        const reducer = (count, site) => isSiteActive(site) ? count + 1 : count
-        return sites.reduce(reducer, 0)
-    }
-
-    const activeSitesPercentage = () => 100 * (activeSitesCount() / sites.length).toFixed(2)
-
-    useEffect(() => {
-        if (sites) {
-            const reducer = (counts, site) => {
-                const { patientsConsentedCount, patientsEnrolledCount, patientsWithdrawnCount, patientsExpectedCount } = site
-                return ({
-                    consented: counts.consented + parseInt(patientsConsentedCount || 0),
-                    enrolled: counts.enrolled + parseInt(patientsEnrolledCount || 0),
-                    withdrawn: counts.withdrawn + parseInt(patientsWithdrawnCount || 0),
-                    expected: counts.expected + parseInt(patientsExpectedCount || 0),
-                })
-            }
-            setPatientCounts(sites.reduce(reducer, patientCounts))
-        }
-    }, [sites])
-
-    return (
-        <Card>
-            <CardHeader title="Milestones"/>
-            <CardContent>
-                <Subsubheading align="center">Site Activation</Subsubheading>
-                <SitesActivationPieChart percentage={ activeSitesPercentage() } />
-                <Caption align="center">
-                    { activeSitesCount() } of { sites.length } sites
-                </Caption>
-            </CardContent>
-            <CardContent>
-                <List dense>
-                    <ListItem>
-                        <ListItemText primary="First IRB Approved" secondary={ firstIRBApprovedDate } />
-                    </ListItem>
-                    <ListItem>
-                        <ListItemText primary="First Site Activated" secondary={ firstSiteActivationDate } />
-                    </ListItem>
-                    <ListItem>
-                        <ListItemText primary="First Subject Enrolled" secondary={ firstSubjectEnrolled } />
-                    </ListItem>
-                    <ListItem>
-                        <ListItemText primary="25% Enrolled" secondary={ siteActivationPercentages[0] } />
-                    </ListItem>
-                    <ListItem>
-                        <ListItemText primary="50% Enrolled" secondary={ siteActivationPercentages[1] } />
-                    </ListItem>
-                    <ListItem>
-                        <ListItemText primary="75% Enrolled" secondary={ siteActivationPercentages[2] } />
-                    </ListItem>
-                    <ListItem>
-                        <ListItemText primary="100% Enrolled" secondary={ siteActivationPercentages[3] } />
-                    </ListItem>
-                </List>
-            </CardContent>
-        </Card>
-    )
-}
-
 export const StudyReportPage = props => {
     const proposalId = props.match.params.proposalID
     const [store, ] = useContext(StoreContext)
@@ -204,7 +99,7 @@ export const StudyReportPage = props => {
                                 <CardContent>
                                     {
                                         studyEnrollmentData && studyEnrollmentData.length > 0
-                                            ? <StudyEnrollment study={ study } sites={ studySites }/>
+                                            ? <StudyEnrollment enrollmentData={ studyEnrollmentData }/>
                                             : <Paragraph>No enrollment information found! <NavLink to={ `${ proposalId }/uploads` }>Upload it</NavLink>!</Paragraph>
                                     }
                                 </CardContent>
