57d7778d0cae8f1da9e23d6858a977eb5a7416f7
Matt Watson <mattwat@gmail.com>
2020-01-06 12:51:39 -0500

    fix padding on chart menu component

--- frontend/src/components/Menus/ChartOptions.js
+++ frontend/src/components/Menus/ChartOptions.js
@@ -15,38 +15,38 @@ export const ChartOptions = (props) => {
 
     return (
         <div style={{ textAlign: 'center', }}>
-            <div style={{ display: 'flex' }}>
-                <ToggleButtonGroup value={ currentSorting } exclusive onChange={ sortingSelectionHandler } style={{ marginRight: '1rem' }}>
-                    <ToggleButton value="alpha">
-                        <Tooltip title="Sort Alphabetically">
-                            <AlphaSortIcon />
-                        </Tooltip>
-                    </ToggleButton>
-                    <ToggleButton value="value">
-                        <Tooltip title="Sort by Value">
-                            <ValueSortIcon />
-                        </Tooltip>
-                    </ToggleButton>
-                </ToggleButtonGroup>
-
-                <ToggleButtonGroup value={ currentType } exclusive onChange={ typeSelectionHandler }>
-                    <ToggleButton value="pie">
-                        <Tooltip title="View as Pie Chart">
-                            <PieChartIcon />
-                        </Tooltip>
-                    </ToggleButton>
-                    <ToggleButton value="bar">
-                        <Tooltip title="View as Bar Chart">
-                            <BarChartIcon />
-                        </Tooltip>
-                    </ToggleButton>
-                </ToggleButtonGroup>
-            </div>
             <FormControlLabel
                 control={ <Switch checked={ hideEmptyGroups } onChange={ toggleHideEmptyGroupsHandler } /> }
                 label="Hide Empty Groups"
                 labelPlacement="start"
+                style={{ marginRight: '1rem' }}
             />
+
+            <ToggleButtonGroup value={ currentSorting } exclusive onChange={ sortingSelectionHandler } style={{ marginRight: '1rem' }}>
+                <ToggleButton value="alpha">
+                    <Tooltip title="Sort Alphabetically">
+                        <AlphaSortIcon />
+                    </Tooltip>
+                </ToggleButton>
+                <ToggleButton value="value">
+                    <Tooltip title="Sort by Value">
+                        <ValueSortIcon />
+                    </Tooltip>
+                </ToggleButton>
+            </ToggleButtonGroup>
+
+            <ToggleButtonGroup value={ currentType } exclusive onChange={ typeSelectionHandler } style={{ marginRight: '1rem' }}>
+                <ToggleButton value="pie">
+                    <Tooltip title="View as Pie Chart">
+                        <PieChartIcon />
+                    </Tooltip>
+                </ToggleButton>
+                <ToggleButton value="bar">
+                    <Tooltip title="View as Bar Chart">
+                        <BarChartIcon />
+                    </Tooltip>
+                </ToggleButton>
+            </ToggleButtonGroup>
        </div>
     )
 }
