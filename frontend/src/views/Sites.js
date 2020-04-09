07c35e6e8468e858b0b8b0336d1850382f976d42
Matt Watson <mattwat@gmail.com>
2020-04-09 13:26:13 -0400

    reduce complexity of api andpoint strings

--- frontend/src/views/Sites.js
+++ frontend/src/views/Sites.js
@@ -86,7 +86,7 @@ export const SitesPage = (props) => {
                     <Title>Sites</Title>
                 </Grid>
                 <Grid item xs={ 11 } md={ 5 }>
-                    <DropZone endpoint={ `${ api.uploadSites }` } />
+                    <DropZone endpoint={ api.uploadSites } />
                 </Grid>
                 <Grid item xs={ 1 } style={{ textAlign: 'right' }}>
                     <DownloadButton path={ api.download('sites')} tooltip="Download Sites CSV Template" />
