2595a7627c489217f944e7733a3738d2500f6823
Matt Watson <mattwat@gmail.com>
2020-09-03 09:43:09 -0400

    remove faulty parts of day-stats component

--- frontend/src/components/Widgets/DayStats.js
+++ frontend/src/components/Widgets/DayStats.js
@@ -116,13 +116,13 @@ export const DayStats = props => {
             }
         >
             <Grid container>
-                <Grid item xs={ 12 } md={ 6 }>
+                <Grid item xs={ 12 }>
                     <Typography variant="h4">Average Number of Days</Typography>
                     <br/><br/>
                     <div style={{ height: '180px' }}>
                         <ResponsiveBar
                             data={[
-                                { timespan: 'Grant Submission to Grant Award',    days: averageDays.grantSubmissionToGrantAward[0],    count: averageDays.grantSubmissionToGrantAward[1]},
+                                // { timespan: 'Grant Submission to Grant Award',    days: averageDays.grantSubmissionToGrantAward[0],    count: averageDays.grantSubmissionToGrantAward[1]},
                                 { timespan: 'Submission to Grant Submission',     days: averageDays.submissionToGrantSubmission[0],    count: averageDays.submissionToGrantSubmission[1]},
                                 { timespan: 'PAT Approval to Grant Submission',   days: averageDays.approvalToGrantSubmission[0],      count: averageDays.approvalToGrantSubmission[1]},
                                 { timespan: 'Submission to PAT Approval',         days: averageDays.submsisionToPatApproval[0],        count: averageDays.submsisionToPatApproval[1]},
@@ -151,47 +151,6 @@ export const DayStats = props => {
                                 <ChartTooltip color={ color }>
                                     <div><strong>{ indexValue }</strong></div>
                                     <div>~ { value } Day{ value !==  1 ? 's' : null }</div>
-                                    <div style={{ opacity: 0.5, fontSize: '90%' }}><small>Calculated from { data.count } Proposals</small></div>
-                                </ChartTooltip>
-                            )} 
-                        />
-                    </div>
-                </Grid>
-                <Grid item xs={ 12 } md={ 6 }>
-                    <Typography variant="h4">Median Number of Days</Typography>
-                    <br/>
-                    <div style={{ height: '180px' }}>
-                        <ResponsiveBar
-                            data={[
-                                { timespan: 'Grant Submission to Grant Award',    days: medianDays.grantSubmissionToGrantAward[0],    count: medianDays.grantSubmissionToGrantAward[1]},
-                                { timespan: 'Submission to Grant Submission',     days: medianDays.submissionToGrantSubmission[0],    count: medianDays.submissionToGrantSubmission[1]},
-                                { timespan: 'PAT Approval to Grant Submission',   days: medianDays.approvalToGrantSubmission[0],      count: medianDays.approvalToGrantSubmission[1]},
-                                { timespan: 'Submission to PAT Approval',         days: medianDays.submsisionToPatApproval[0],        count: medianDays.submsisionToPatApproval[1]},
-                            ]}
-                            keys={ ['days'] }
-                            indexBy="timespan"
-                            margin={{ top: 0, right: 32, bottom: 0, left: 216 }}
-                            layout="horizontal"
-                            enableGridY={ false }
-                            padding={ 0.1 }
-                            colors={ theme.palette.chartColors }
-                            colorBy="index"
-                            borderColor="inherit:darker(1.6)"
-                            axisTop={ null }
-                            axisRight={ null }
-                            axisBottom={ null }
-                            axisLeft={{ tickSize: 0, tickPadding: 16, tickRotation: 0, legend: '', legendPosition: 'middle', legendOffset: 0 }}
-                            labelSkipWidth={ 50 }
-                            labelFormat={ d => `${ d } days` }
-                            labelTextColor="inherit:darker(1.6)"
-                            animate={ true }
-                            motionStiffness={ 90 }
-                            motionDamping={ 15 }
-                            legends={ [] }
-                            tooltip={ ({ id, value, color, indexValue, data }) => (
-                                <ChartTooltip color={ color }>
-                                    <div><strong>{ indexValue }</strong></div>
-                                    <div>{ value } Day{ value !==  1 ? 's' : null }</div>
                                     <div style={{ opacity: 0.5, fontSize: '90%' }}><small>Calculated from { data.count } Proposals</small></div>
                                 </ChartTooltip>
                             )} 
