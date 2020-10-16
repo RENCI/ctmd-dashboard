4e3f30958bbe2ab817f1f8d5bbe721bdd0bbbc22
Matt Watson <mattwat@gmail.com>
2020-10-15 22:49:22 -0400

    just set valid referrer to true

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -16,7 +16,7 @@ export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState()
     const [localStorageUser, setLocalStorageUser] = useLocalStorage('ctmd-user')
     // const validReferrer = document.referrer.includes('redcap.vanderbilt.edu') || process.env.NODE_ENV === 'development'
-    const validReferrer = document.referrer.includes('redcap.vanderbilt.edu')
+    const validReferrer = true
 
     const logout = () => {
         localStorage.removeItem('ctmd-user')
