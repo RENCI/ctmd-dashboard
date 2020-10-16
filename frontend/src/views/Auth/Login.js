b66f3564e72911367987e184d0cfdb669bc22199
Matt Watson <mattwat@gmail.com>
2020-10-16 11:16:22 -0400

    add login instructions

--- frontend/src/views/Auth/Login.js
+++ frontend/src/views/Auth/Login.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react'
 import { Title, Paragraph } from '../../components/Typography'
-import { Card, CardHeader, CardContent, Button } from '@material-ui/core'
+import { Card, CardHeader, CardContent, Button, List, ListItem } from '@material-ui/core'
 
 export const LoginPage = props => {
     return (
@@ -11,18 +11,20 @@ export const LoginPage = props => {
                 <CardHeader title="Login" />
                 <CardContent>
                     <Paragraph>
-                        You must be logged in to the <a href="https://redcap.vanderbilt.edu/plugins/TIN/user/login" rel="noopener noreferrer">TIN Dashboard</a> to view this site.
+                        You must be logged in to the TIN Dashboard to view this site.
                     </Paragraph>
-                    <Paragraph>
-                        Click the button below to authenticate if you are already logged in to the TIN Dashboard.
-                    </Paragraph>
+                    <ol>
+                        <li>Log into the TIN Dashboard if you are not already.</li>
+                        <li>Navigate back to this page.</li>
+                        <li>Click the "Authenticate with TIN" button below to gain access to this site.</li>
+                    </ol>
                     <Paragraph center>
                         <form
                             method="POST"
                             action={ `https://redcap.vanderbilt.edu/plugins/TIN/sso/send_login?target-url=${ window.location.origin }/api/auth` }
                             style={{ display: 'flex', justifyContent: 'center' }} 
                         >
-                            <Button type="submit" color="primary" variant="contained">Authenticate</Button>
+                            <Button type="submit" color="primary" variant="contained">Authenticate with TIN</Button>
                         </form>
                     </Paragraph>
                 </CardContent>
@@ -30,3 +32,8 @@ export const LoginPage = props => {
         </Fragment>
     )
 }
+
+// open a new tab
+// log into the tin Dashboard
+// come back to stage
+// click authenticate
