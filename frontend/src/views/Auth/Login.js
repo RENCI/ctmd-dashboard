bcadf939e95234a3165b905b270db0c220163665
Matt Watson <mattwat@gmail.com>
2020-10-15 22:49:37 -0400

    add check access button to access denied view

--- frontend/src/views/Auth/Login.js
+++ frontend/src/views/Auth/Login.js
@@ -12,6 +12,12 @@ export const LoginPage = props => {
                 <CardContent>
                     <Paragraph>
                         Please login to the <a href="https://redcap.vanderbilt.edu/plugins/TIN/user/login" rel="noopener noreferrer">TIN Dashboard</a> to view this page.
+                        Click the button below if you are already logged in to the TIN Dashboard.
+                    </Paragraph>
+                    <Paragraph center>
+                        <form method="POST" action={ `https://redcap.vanderbilt.edu/plugins/TIN/sso/send_login?target-url=${ window.location.host }/auth` }>
+                          <input type="submit" class="login-button" value="Login" />
+                        </form>
                     </Paragraph>
                 </CardContent>
           </Card>
