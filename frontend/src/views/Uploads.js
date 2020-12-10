28f9d606d6c247c2458c883151b1544956667383
Luthfi Bustillos <luthfi@rciadmins-MBP.lan>
2020-12-10 12:58:25 -0500

    add ability to download csv template files in the uploads view

--- frontend/src/views/Uploads.js
+++ frontend/src/views/Uploads.js
@@ -3,69 +3,80 @@ import api from '../Api'
 import { Grid, Card, CardHeader, CardContent } from '@material-ui/core'
 import { Title, Subheading } from '../components/Typography'
 import { DropZone } from '../components/Forms'
+import { DownloadButton } from '../components/Forms'
+import { makeStyles } from '@material-ui/styles'
+
+const useStyles = makeStyles(theme => ({
+    action: {
+        height: 'initial'
+    }
+}))
+
 
 export const UploadsPage = props => {
+    const classes = useStyles()
+    console.log(classes)
     return (
         <div>
             <Title>Uploads</Title>
 
-            <Grid container spacing={ 8 }>
+            <Grid container spacing={8}>
 
-                <Grid item xs={ 12 }>
+                <Grid item xs={12}>
                     <Subheading>Global Uploads</Subheading>
                 </Grid>
 
-                <Grid item xs={ 12 } md={ 6 }>
+                <Grid item xs={12} md={6}>
                     <Card>
-                        <CardHeader title="Upload Sites" />
+                        <CardHeader title="Upload Sites" classes={{ action: classes.action }} action={<DownloadButton path={api.download('sites')} tooltip="Download Sites CSV Template" />} />
                         <CardContent>
-                            <DropZone method="POST" endpoint={ api.uploadSites } />
+                            <DropZone method="POST" endpoint={api.uploadSites} />
                         </CardContent>
                     </Card>
                 </Grid>
 
-                <Grid item xs={ 12 } md={ 6 }>
+                <Grid item xs={12} md={6}>
                     <Card>
-                        <CardHeader title="Upload CTSAs" />
+                        <CardHeader title="Upload CTSAs" classes={{ action: classes.action }} action={<DownloadButton path={api.download('ctsas')} tooltip="Download CTSAs CSV Template" />} />
                         <CardContent>
-                            <DropZone method="POST" endpoint={ api.uploadCtsas } />
+                            <DropZone method="POST" endpoint={api.uploadCtsas} />
                         </CardContent>
                     </Card>
                 </Grid>
 
             </Grid>
 
-            <br/><br/><br/>
-                
-            <Grid container spacing={ 8 }>
+            <br /><br /><br />
 
-                <Grid item xs={ 12 }>
+            <Grid container spacing={8}>
+
+                <Grid item xs={12}>
                     <Subheading>Per-Study Uploads</Subheading>
                 </Grid>
 
-                <Grid item xs={ 12 } md={ 4 }>
+                <Grid item xs={12} md={4}>
                     <Card>
-                        <CardHeader title="Upload Study Profile" />
+                        <CardHeader title="Upload Study Profile" classes={{ action: classes.action }} action={<DownloadButton path={api.download('study-profile')} tooltip="Download Study Profile CSV Template" />} />
                         <CardContent>
-                            <DropZone method="POST" endpoint={ api.uploadStudyProfile } />
+                            <DropZone method="POST" endpoint={api.uploadStudyProfile} />
                         </CardContent>
                     </Card>
                 </Grid>
 
-                <Grid item xs={ 12 } md={ 4 }>
+                <Grid item xs={12} md={4}>
                     <Card>
-                        <CardHeader title="Upload Study Sites" />
+                        <CardHeader title="Upload Study Sites" classes={{ action: classes.action }} action={<DownloadButton path={api.download('study-sites')} tooltip="Download Study Sites CSV Template" />}/>
                         <CardContent>
-                            <DropZone method="POST" endpoint={ api.uploadStudySites } />
+                            <DropZone method="POST" endpoint={api.uploadStudySites} />
                         </CardContent>
                     </Card>
                 </Grid>
 
-                <Grid item xs={ 12 } md={ 4 }>
+                <Grid item xs={12} md={4}>
                     <Card>
-                        <CardHeader title="Upload Study Enrollment Data" />
+                        <CardHeader title="Upload Study Enrollment Data" classes={{ action: classes.action }} action={<DownloadButton path={api.download('enrollment')} tooltip="Download Enrollment CSV Template" />} />
                         <CardContent>
-                            <DropZone method="POST" endpoint={ api.uploadStudyEnrollmentData } />
+                            <DropZone method="POST" endpoint={api.uploadStudyEnrollmentData} />
                         </CardContent>
                     </Card>
                 </Grid>
