f76bb46599ce0bbe6aa0b2e1a200b93b44d10648
luthfib <luthfibustillos@gmail.com>
2020-12-23 14:24:08 -0500

    add eslint & format couple file to show new initial rules

--- frontend/src/views/Sites.js
+++ frontend/src/views/Sites.js
@@ -1,104 +1,123 @@
-import React, { useContext, useEffect, useState } from 'react'
-import axios from 'axios'
-import api from '../Api'
-import { Title } from '../components/Typography'
-import { CircularLoader } from '../components/Progress/Progress'
-import { SitesEnrollmentTable } from '../components/Tables/SitesEnrollmentTable'
-import { StoreContext } from '../contexts/StoreContext'
-import { Grid } from '@material-ui/core'
-import { DropZone } from '../components/Forms/DropZone'
-import { DownloadButton } from '../components/Forms'
-import { DataUploadHelper } from '../components/Helper'
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import api from "../Api";
+import { Title } from "../components/Typography";
+import { CircularLoader } from "../components/Progress/Progress";
+import { SitesEnrollmentTable } from "../components/Tables/SitesEnrollmentTable";
+import { StoreContext } from "../contexts/StoreContext";
+import { Grid } from "@material-ui/core";
+import { DropZone } from "../components/Forms/DropZone";
+import { DownloadButton } from "../components/Forms";
+import { DataUploadHelper } from "../components/Helper";
 
 export const SitesPage = (props) => {
-    const [store, ] = useContext(StoreContext)
-    const [studyNames, setStudyNames] = useState(null);
-    const [sites, setSites] = useState([])
-    const [studySites, setStudySites] = useState([]);
-    const [tableData, setTableData] = useState(null);
+  const [store] = useContext(StoreContext);
+  const [studyNames, setStudyNames] = useState(null);
+  const [sites, setSites] = useState([]);
+  const [studySites, setStudySites] = useState([]);
+  const [tableData, setTableData] = useState(null);
 
-    // Get sites
-    useEffect(() => {
-        const fetchSites = async () => {
-            await axios.get(api.sites)
-                .then(response => {
-                  setSites(response.data)
-                })
-                .catch(error => console.error(error))
-        }
-        fetchSites()
-    }, [])
-
-    // Get study sites
-    useEffect(() => {
-        if (store.proposals) {
-            const studies = store.proposals.filter(proposal => proposal.profile && true)
-
-            const names = {}
-            studies.forEach(study => {
-              names[study.proposalID] = study.shortTitle
-            })
-
-            setStudyNames(names);
-
-            const fetchStudyData = async (studies) => {
-                await axios.all(
-                  studies.map(study => axios.get(api.studySites(study.proposalID)))
-                )
-                .then(response => {
-                    setStudySites(response.map(study => study.data).flat())
-                })
-            }
-
-            fetchStudyData(studies)
-        }
-    }, [store.proposals])
-
-    // Create table data
-    useEffect(() => {
-      setTableData(sites.map(site => {
-        const studies = studySites.filter(({ siteId }) => site.siteId === siteId)
-
-        return studies.length === 0 ? {
-            id: site.siteId,
-            name: site.siteName,
-            studyName: "None",
-            ctsaId: site.ctsaId
-        } : studies.map(studySite => {
-            const enrolled = +studySite.patientsEnrolledCount
-            const expected = +studySite.patientsExpectedCount
-            const percent = expected === 0 ? 0 : enrolled / expected * 100;
-
-            return {
-                id: site.siteId,
-                name: studySite.siteName,
-                studyName: studyNames[studySite.ProposalID],
-                enrolled: enrolled,
-                expected: expected,
-                percentEnrolled: Math.round(percent),
-                ctsaId: site.ctsaId
-            }
+  // Get sites
+  useEffect(() => {
+    const fetchSites = async () => {
+      await axios
+        .get(api.sites)
+        .then((response) => {
+          setSites(response.data);
         })
-      }).flat())
-    }, [sites, studySites])
+        .catch((error) => console.error(error));
+    };
+    fetchSites();
+  }, []);
 
-    return (
-        <div>
-            <Grid container>
-                <Grid item xs={ 11 } md={ 6 }>
-                    <Title>Sites</Title>
-                </Grid>
-                <Grid item xs={ 11 } md={ 5 }>
-                    <DropZone endpoint={ api.uploadSites } method="POST" />
-                </Grid>
-                <Grid item xs={ 1 } style={{ textAlign: 'right' }}>
-                    <DownloadButton path={ api.download('sites')} tooltip="Download Sites CSV Template" />
-                    <DataUploadHelper />
-                </Grid>
-            </Grid>
+  // Get study sites
+  useEffect(() => {
+    if (store.proposals) {
+      const studies = store.proposals.filter(
+        (proposal) => proposal.profile && true
+      );
 
-            { tableData ? <SitesEnrollmentTable data={ tableData } /> : <CircularLoader /> }
+      const names = {};
+      studies.forEach((study) => {
+        names[study.proposalID] = study.shortTitle;
+      });
 
-        </div>
-    )
-}
+      setStudyNames(names);
+
+      const fetchStudyData = async (studies) => {
+        await axios
+          .all(
+            studies.map((study) => axios.get(api.studySites(study.proposalID)))
+          )
+          .then((response) => {
+            setStudySites(response.map((study) => study.data).flat());
+          });
+      };
+
+      fetchStudyData(studies);
+    }
+  }, [store.proposals]);
+
+  // Create table data
+  useEffect(() => {
+    setTableData(
+      sites
+        .map((site) => {
+          const studies = studySites.filter(
+            ({ siteId }) => site.siteId === siteId
+          );
+
+          return studies.length === 0
+            ? {
+                id: site.siteId,
+                name: site.siteName,
+                studyName: "None",
+                ctsaId: site.ctsaId,
+              }
+            : studies.map((studySite) => {
+                const enrolled = +studySite.patientsEnrolledCount;
+                const expected = +studySite.patientsExpectedCount;
+                const percent =
+                  expected === 0 ? 0 : (enrolled / expected) * 100;
+
+                return {
+                  id: site.siteId,
+                  name: studySite.siteName,
+                  studyName: studyNames[studySite.ProposalID],
+                  enrolled: enrolled,
+                  expected: expected,
+                  percentEnrolled: Math.round(percent),
+                  ctsaId: site.ctsaId,
+                };
+              });
+        })
+        .flat()
+    );
+  }, [sites, studySites]);
+
+  return (
+    <div>
+      <Grid container>
+        <Grid item xs={11} md={6}>
+          <Title>Sites</Title>
+        </Grid>
+        <Grid item xs={11} md={5}>
+          <DropZone endpoint={api.uploadSites} method="POST" />
+        </Grid>
+        <Grid item xs={1} style={{ textAlign: "right" }}>
+          <DownloadButton
+            path={api.download("sites")}
+            tooltip="Download Sites CSV Template"
+          />
+          <DataUploadHelper />
+        </Grid>
+      </Grid>
+
+      {tableData ? (
+        <SitesEnrollmentTable data={tableData} />
+      ) : (
+        <CircularLoader />
+      )}
+    </div>
+  );
+};
--- frontend/src/views/Uploads.js
+++ frontend/src/views/Uploads.js
@@ -6,81 +6,100 @@ import { DropZone } from '../components/Forms'
 import { DownloadButton } from '../components/Forms'
 import { makeStyles } from '@material-ui/styles'
 
-const useStyles = makeStyles(theme => ({
-    action: {
-        height: 'initial'
-    }
+const useStyles = makeStyles((theme) => ({
+  action: {
+    height: 'initial',
+  },
 }))
 
+export const UploadsPage = (props) => {
+  const classes = useStyles()
+  return (
+    <div>
+      <Title>Uploads</Title>
 
-export const UploadsPage = props => {
-    const classes = useStyles()
-    return (
-        <div>
-            <Title>Uploads</Title>
+      <Grid container spacing={8}>
+        <Grid item xs={12}>
+          <Subheading>Global Uploads</Subheading>
+        </Grid>
 
-            <Grid container spacing={8}>
+        <Grid item xs={12} md={6}>
+          <Card>
+            <CardHeader
+              title="Upload Sites"
+              classes={{ action: classes.action }}
+              action={<DownloadButton path={api.download('sites')} tooltip="Download Sites CSV Template" />}
+            />
+            <CardContent>
+              <DropZone method="POST" endpoint={api.uploadSites} />
+            </CardContent>
+          </Card>
+        </Grid>
 
-                <Grid item xs={12}>
-                    <Subheading>Global Uploads</Subheading>
-                </Grid>
+        <Grid item xs={12} md={6}>
+          <Card>
+            <CardHeader
+              title="Upload CTSAs"
+              classes={{ action: classes.action }}
+              action={<DownloadButton path={api.download('ctsas')} tooltip="Download CTSAs CSV Template" />}
+            />
+            <CardContent>
+              <DropZone method="POST" endpoint={api.uploadCtsas} />
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
 
-                <Grid item xs={12} md={6}>
-                    <Card>
-                        <CardHeader title="Upload Sites" classes={{ action: classes.action }} action={<DownloadButton path={api.download('sites')} tooltip="Download Sites CSV Template" />} />
-                        <CardContent>
-                            <DropZone method="POST" endpoint={api.uploadSites} />
-                        </CardContent>
-                    </Card>
-                </Grid>
+      <br />
+      <br />
+      <br />
 
-                <Grid item xs={12} md={6}>
-                    <Card>
-                        <CardHeader title="Upload CTSAs" classes={{ action: classes.action }} action={<DownloadButton path={api.download('ctsas')} tooltip="Download CTSAs CSV Template" />} />
-                        <CardContent>
-                            <DropZone method="POST" endpoint={api.uploadCtsas} />
-                        </CardContent>
-                    </Card>
-                </Grid>
+      <Grid container spacing={8}>
+        <Grid item xs={12}>
+          <Subheading>Per-Study Uploads</Subheading>
+        </Grid>
 
-            </Grid>
+        <Grid item xs={12} md={4}>
+          <Card>
+            <CardHeader
+              title="Upload Study Profile"
+              classes={{ action: classes.action }}
+              action={
+                <DownloadButton path={api.download('study-profile')} tooltip="Download Study Profile CSV Template" />
+              }
+            />
+            <CardContent>
+              <DropZone method="POST" endpoint={api.uploadStudyProfile} />
+            </CardContent>
+          </Card>
+        </Grid>
 
-            <br /><br /><br />
+        <Grid item xs={12} md={4}>
+          <Card>
+            <CardHeader
+              title="Upload Study Sites"
+              classes={{ action: classes.action }}
+              action={<DownloadButton path={api.download('study-sites')} tooltip="Download Study Sites CSV Template" />}
+            />
+            <CardContent>
+              <DropZone method="POST" endpoint={api.uploadStudySites} />
+            </CardContent>
+          </Card>
+        </Grid>
 
-            <Grid container spacing={8}>
-
-                <Grid item xs={12}>
-                    <Subheading>Per-Study Uploads</Subheading>
-                </Grid>
-
-                <Grid item xs={12} md={4}>
-                    <Card>
-                        <CardHeader title="Upload Study Profile" classes={{ action: classes.action }} action={<DownloadButton path={api.download('study-profile')} tooltip="Download Study Profile CSV Template" />} />
-                        <CardContent>
-                            <DropZone method="POST" endpoint={api.uploadStudyProfile} />
-                        </CardContent>
-                    </Card>
-                </Grid>
-
-                <Grid item xs={12} md={4}>
-                    <Card>
-                        <CardHeader title="Upload Study Sites" classes={{ action: classes.action }} action={<DownloadButton path={api.download('study-sites')} tooltip="Download Study Sites CSV Template" />}/>
-                        <CardContent>
-                            <DropZone method="POST" endpoint={api.uploadStudySites} />
-                        </CardContent>
-                    </Card>
-                </Grid>
-
-                <Grid item xs={12} md={4}>
-                    <Card>
-                        <CardHeader title="Upload Study Enrollment Data" classes={{ action: classes.action }} action={<DownloadButton path={api.download('enrollment')} tooltip="Download Enrollment CSV Template" />} />
-                        <CardContent>
-                            <DropZone method="POST" endpoint={api.uploadStudyEnrollmentData} />
-                        </CardContent>
-                    </Card>
-                </Grid>
-            </Grid>
-
-        </div>
-    )
+        <Grid item xs={12} md={4}>
+          <Card>
+            <CardHeader
+              title="Upload Study Enrollment Data"
+              classes={{ action: classes.action }}
+              action={<DownloadButton path={api.download('enrollment')} tooltip="Download Enrollment CSV Template" />}
+            />
+            <CardContent>
+              <DropZone method="POST" endpoint={api.uploadStudyEnrollmentData} />
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </div>
+  )
 }
