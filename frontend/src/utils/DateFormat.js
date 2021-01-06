5bb42d78d7a5a8750cdb06cefa0c29de119d5b38
luthfib <luthfibustillos@gmail.com>
2021-01-06 10:49:24 -0500

    format changes

--- frontend/src/utils/DateFormat.js
+++ frontend/src/utils/DateFormat.js
@@ -1,10 +1,18 @@
 const monthNames = [
-  "January", "February", "March",
-  "April", "May", "June",
-  "July", "August", "September",
-  "October", "November", "December"
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
 ]
 
-export const formatDate = (date, options = {year: 'numeric', month: 'long', day: '2-digit'}, locales='en-US' ) => {
-  return date.toLocaleDateString(locales, options);
+export const formatDate = (date, options = { year: 'numeric', month: 'long', day: '2-digit' }, locales = 'en-US') => {
+  return date.toLocaleDateString(locales, options)
 }
