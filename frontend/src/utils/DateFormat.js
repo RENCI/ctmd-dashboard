351da3d2eea5e9f77694fb54f00b21cc26645a39
Matt Watson <mattwat@gmail.com>
2021-09-02 11:11:48 -0400

    fix timezone offset in format date util function

--- frontend/src/utils/DateFormat.js
+++ frontend/src/utils/DateFormat.js
@@ -13,6 +13,6 @@ const monthNames = [
   'December',
 ]
 
-export const formatDate = (date, options = { year: 'numeric', month: 'long', day: '2-digit' }, locales = 'en-US') => {
+export const formatDate = (date, options = { year: 'numeric', month: 'long', day: '2-digit', timeZone: 'UTC' }, locales = 'en-US') => {
   return date.toLocaleDateString(locales, options)
 }
