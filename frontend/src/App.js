f7c31af1a57f957e7f94aada13d7d0601b48dbf2
Matt Watson <mwatson@renci.org>
2022-04-29 11:42:56 -0400

    pass ga tracking id in as env var

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import { ThemeProvider } from '@material-ui/styles'
 import { AuthProvider, SettingsProvider, StoreProvider, FlashMessageProvider } from './contexts'
 import Analytics from 'react-ga4'
 
-const TRACKING_ID = 'G-JEXX407FCJ'
+const TRACKING_ID = process.env.REACT_APP_GA4_TRACKING_ID || ''
 Analytics.initialize(TRACKING_ID)
 
 const App = props => {
