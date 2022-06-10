cd73627228ca2b4f7e038c9ac239ffb18995e50f
Matt Watson <mwatson@renci.org>
2022-06-10 15:51:47 -0400

    return CTSAs with IDs as integers

--- api/controllers/ctsas.js
+++ api/controllers/ctsas.js
@@ -4,7 +4,13 @@ exports.list = (req, res) => {
     const query = `SELECT * FROM "CTSAs";`
     db.any(query)
         .then(data => {
-            res.status(200).send(data)
+            const filteredCtsas = data
+                .filter(d => parseInt(d.ctsaId))
+                .map(d => ({
+                    ctsaId: +d.ctsaId,
+                    ctsaName: d.ctsaName
+                }))
+            res.status(200).send(filteredCtsas)
         })
         .catch(error => {
             console.log('ERROR:', error)
