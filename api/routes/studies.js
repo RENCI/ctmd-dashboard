4bcb21a8bbdcaf20077594a40269ced91db2b4fd
Vladimir <117946722+vladimir2217@users.noreply.github.com>
2022-12-12 16:53:06 -0500

    #516 Studies with VERY large enrollment display NaN% for total enrollment number

--- api/routes/studies.js
+++ api/routes/studies.js
@@ -5,6 +5,7 @@ const studiesController = require('../controllers/studies')
 // Routes beginning with "HOSTNAME/studies/..."
 
 router.route('/:id(\\d+)').get(studiesController.getProfile)
+router.route('/:id(\\d+)/sites').get(studiesController.getSites)
 router.route('/studysites').get(studiesController.getStudySites)
 router.route('/:id(\\d+)/enrollment-data').get(studiesController.getEnrollmentData)
 
