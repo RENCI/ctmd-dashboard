9703a98174a95af97b79933afe9e022cf1d96884
Matt Watson <mwatson@renci.org>
2023-01-09 11:14:29 -0500

    filter proposals and dump profiles to console

--- frontend/src/components/Forms/StudiesDownload.js
+++ frontend/src/components/Forms/StudiesDownload.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { IconButton } from '@material-ui/core'
 import { DownloadIcon } from '../Icons/Download'
+import { useStore } from '../../contexts'
 
 export const StudiesDownloadForm = props => {
+    const [store, ] = useStore()
+
+    const { proposals } = store
+    const profiles = useMemo(() => {
+        return proposals
+            .filter(p => p.profile !== null)
+            .map(p => p.profile)
+    }, [store.proposals])
+
     const handleClickDownload = () => {
-        console.log('download all studies')
+        console.log(profiles)
     }
 
     return (
