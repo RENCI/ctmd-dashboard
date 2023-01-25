ec41d7a0746df6249dc9fd1650edcd51795abb48
Vladimir <vgl@ad.unc.edu>
2023-01-25 14:26:54 -0500

    testing if change will propagate

--- frontend/src/components/Tables/LookupTable.js
+++ frontend/src/components/Tables/LookupTable.js
@@ -15,7 +15,7 @@ export const LookupTable = ({ title, data }) => {
             columns={ [
                 { title: 'ID', field: 'id', },
                 { title: 'Name2222', field: 'name', },
-                { title: 'Action', hidden: true, render: RemoveBtn },
+                { title: 'Action', render: RemoveBtn },
             ] }
             data={ data }
             options={{
