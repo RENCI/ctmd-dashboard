004594c34075cfb6e5d8e7e1d1d265d86e857d0d
Vladimir <vgl@ad.unc.edu>
2023-01-25 12:52:35 -0500

    testing if change will propagate

--- frontend/src/components/Tables/LookupTable.js
+++ frontend/src/components/Tables/LookupTable.js
@@ -10,7 +10,7 @@ export const LookupTable = ({ title, data }) => {
     const now = new Date()
     return (
         <div>
-            <div>HHH100</div>
+            <div>HHH1001</div>
         <MaterialTable
             columns={ [
                 { title: 'ID', field: 'id', },
