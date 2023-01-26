b1013727015f839d7b2951f773b0d6400fbc3dee
Vladimir <vgl@ad.unc.edu>
2023-01-26 16:00:53 -0500

    added hidden back

--- frontend/src/components/Tables/LookupTable.js
+++ frontend/src/components/Tables/LookupTable.js
@@ -13,7 +13,7 @@ export const LookupTable = ({ title, data }) => {
             columns={ [
                 { title: 'ID', field: 'id', },
                 { title: 'Name', field: 'name', },
-                { title: 'Action', render: RemoveBtn },
+                { title: 'Action', render: RemoveBtn, hidden: true },
             ] }
             data={ data }
             options={{
