d8437700e09e4dd5d12be2ac2e7b77ee7a4da141
Matt Watson <mwatson@renci.org>
2023-02-23 13:42:30 -0500

    align download with study profile view

--- frontend/src/components/Forms/StudiesDownload.js
+++ frontend/src/components/Forms/StudiesDownload.js
@@ -8,12 +8,43 @@ export const StudiesDownloadForm = props => {
     const [{ proposals }, ] = useStore()
 
     const profiles = useMemo(() => {
-        return proposals
-            .filter(p => !!p.profile)
-            .map(p => {
-                const { profile, ...rest } = p
-                return { ...rest, ...p.profile }
-            })
+        const studies = proposals.filter(p => !!p.profile)
+        console.log(studies)
+        return studies
+            .map(p => ({
+                'Proposal ID': p.proposalID,
+                'Short Title': p.shortTitle,
+                'Network': p.profile.network,
+                'Assigned TIC/RIC': p.assignToInstitution,
+                'Type': p.profile.type,
+                'Has Linked Studies': p.profile.linkedStudies,
+                'Design': p.profile.design,
+                'Study is Randomized': p.profile.isRandomized,
+                'Randomization Unit': p.profile.randomizationUnit,
+                'Randomization Feature': p.profile.randomizationFeature,
+                'Ascertainment': p.profile.ascertainment,
+                'Observations': p.profile.observations,
+                'Pilot Study': p.profile.isPilot,
+                'Is Registry?': p.profile.isRegistry,
+                'EHR Data Transfer': p.profile.ehrDataTransfer,
+                'EHR Data Transfer Option': p.profile.ehrDatatransferOption,
+                'Study Requires Consent': p.profile.isConsentRequired,
+                'Is EFIC': p.profile.isEfic,
+                'IRB Type': p.profile.irbType,
+                'Regulatory Classification': p.profile.regulatoryClassification,
+                'ClinicalTrials.gov ID': p.profile.clinicalTrialsGovId,
+                'DSMB/DMC Required': p.profile.isDsmbDmcRequired,
+                'Initial ParticipatingSiteCount': p.profile.initialParticipatingSiteCount,
+                'Enrollment Goal': p.profile.enrollmentGoal,
+                'Initial Projected Enrollment Duration': p.profile.initialProjectedEnrollmentDuration,
+                'Lead PIs': p.profile.leadPIs,
+                'Awardee Site Acronym': p.profile.awardeeSiteAcronym,
+                'Primary Funding Type': p.profile.primaryFundingType,
+                'Funded Primarily by Infrastructure': p.profile.isFundedPrimarilyByInfrastructure,
+                'Was Previously Funded': p.profile.isPreviouslyFunded,
+                'Date Funding was Awarded': p.profile.fundingAwardDate,
+                'Phase': p.phase,
+            }))
     }, [proposals])
 
     return (
