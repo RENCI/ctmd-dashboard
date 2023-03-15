b87bea7d355957f39b73bcd3c1d0a6600f4b5899
Matt Watson <mwatson@renci.org>
2023-03-15 12:59:00 -0400

    styling improvements

--- frontend/src/components/Forms/StudiesDownload.js
+++ frontend/src/components/Forms/StudiesDownload.js
@@ -1,154 +1,174 @@
-import React, { useMemo, useState } from 'react'
-import { Button, Fade, IconButton, Paper, Popper, Tooltip } from '@material-ui/core'
+import React, { Fragment, useMemo, useState } from 'react'
+import {
+  Button, Checkbox, Divider, Fade, FormControlLabel, FormGroup, Paper, Popper, Tooltip, Typography,
+} from '@material-ui/core'
 import { DownloadIcon } from '../Icons/Download'
 import { useStore } from '../../contexts'
 import { CSVLink } from 'react-csv'
 
 export const StudiesDownloadForm = props => {
-    const [popperAnchor, setPopperAnchor] = useState(null)
-    const [open, setOpen] = useState(false)
-    const [{ proposals }, ] = useStore()
-    const [exportFields, setExportFields] = useState({
-        proposals: true,
-        studies: true,
+  const [{ proposals }, ] = useStore()
+
+  const [popperAnchor, setPopperAnchor] = useState(null)
+  const [open, setOpen] = useState(false)
+
+  const [exportFields, setExportFields] = useState({
+    proposals: true,
+    studies: true,
+  })
+
+  const handleToggleExportFields = field => event => {
+    if (!['studies', 'proposals'].includes(field)) {
+      return
+    }
+    setExportFields({
+      ...exportFields,
+      [field]: event.target.checked,
     })
-    const handleToggleExportFields = event => {
-        const { field } = event.target.dataset
-        if (!['studies', 'proposals'].includes(field)) {
-            return
-        }
-        setExportFields({
-            ...exportFields,
-            [event.target.dataset.field]: event.target.checked,
-        })
-    }
+  }
 
-    const reports = useMemo(() => {
-        return proposals
-            .filter(p => !!p.profile)
-            .map(p => ({
-                'Proposal ID': p.proposalID,
-                'Short Title': p.shortTitle,
-                'Network': p.profile.network,
-                'Assigned TIC/RIC': p.assignToInstitution,
-                'Type': p.profile.type,
-                'Has Linked Studies': p.profile.linkedStudies,
-                'Design': p.profile.design,
-                'Study is Randomized': p.profile.isRandomized,
-                'Randomization Unit': p.profile.randomizationUnit,
-                'Randomization Feature': p.profile.randomizationFeature,
-                'Ascertainment': p.profile.ascertainment,
-                'Observations': p.profile.observations,
-                'Pilot Study': p.profile.isPilot,
-                'Is Registry?': p.profile.isRegistry,
-                'EHR Data Transfer': p.profile.ehrDataTransfer,
-                'EHR Data Transfer Option': p.profile.ehrDatatransferOption,
-                'Study Requires Consent': p.profile.isConsentRequired,
-                'Is EFIC': p.profile.isEfic,
-                'IRB Type': p.profile.irbType,
-                'Regulatory Classification': p.profile.regulatoryClassification,
-                'ClinicalTrials.gov ID': p.profile.clinicalTrialsGovId,
-                'DSMB/DMC Required': p.profile.isDsmbDmcRequired,
-                'Initial ParticipatingSiteCount': p.profile.initialParticipatingSiteCount,
-                'Enrollment Goal': p.profile.enrollmentGoal,
-                'Initial Projected Enrollment Duration': p.profile.initialProjectedEnrollmentDuration,
-                'Lead PIs': p.profile.leadPIs,
-                'Awardee Site Acronym': p.profile.awardeeSiteAcronym,
-                'Primary Funding Type': p.profile.primaryFundingType,
-                'Funded Primarily by Infrastructure': p.profile.isFundedPrimarilyByInfrastructure,
-                'Was Previously Funded': p.profile.isPreviouslyFunded,
-                'Date Funding was Awarded': p.profile.fundingAwardDate,
-                'Phase': p.phase,
+  const handleClickOpen = event => {
+    setPopperAnchor(event.currentTarget)
+    setOpen(prev => !prev);
+  }
 
-                // studies table columns
-                'Short Title': p.shortTitle,
-                'PI': p.piName,
-                'Proposal Status': p.proposalStatus,
-                'Therapeutic Area': p.therapeuticArea,
-                'Submitting Institution': p.submitterInstitution,
-                'Assigned TIC/RIC': p.assignToInstitution,
-                'Submission Date': p.dateSubmitted,
-                'PAT Review Date': p.meetingDate,
-                'Planned Grant Submission Date': p.plannedGrantSubmissionDate,
-                'Actual Grant Submission Date': p.actualGrantSubmissionDate,
-                'Grant Award Date': p.actualGrantAwardDate,
-                'Funding Amount': p.fundingAmount,
-                'Funding Period': p.fundingPeriod,
-                
-                // proposals table columns
-                // 'COVID Study': p.covidStudy,
-                // 'fundingStatusWhenApproved': p.fundingStatusWhenApproved,
-                // 'Funding Status': p.fundingStatus,
-                // 'Study Population': p.studyPopulation,
-                // 'Phase': p.phase,
-                // 'Funding Source': p.fundingSource,
-                // 'Funding Insitute 1': p.fundingInstitute,
-                // 'Funding Insitute 2': p.fundingInstitute2,
-                // 'Funding Insitute 3': p.fundingInstitute3,
-                // 'New Funding Source': p.newFundingSource,
-                // 'Funding Source Confirmation': p.fundingSourceConfirmation,
-                // 'Actual Funding Start Date': p.actualFundingStartDate,
-                // 'Estimated Funding Start Date': p.estimatedFundingStartDate,
-                // 'Actual Protocol Final Date': p.actualProtocolFinalDate,
-                // 'Approval Release Diff': p.approvalReleaseDiff,
-                // 'Initial Contact Date': p.firstContact,
-                // 'Kick-off Meeting Date': p.kickOff,
-                // 'Number of CTSA Program Hub Sites': p.numberCTSAprogHubSites,
-                // 'Number of Sites': p.numberSites,
-                // 'Recommend for Comprehensive Consultation': p.approvedForComprehensiveConsultation,
-            }))
-    }, [proposals])
+  const reports = useMemo(() => {
+    return proposals
+      .filter(p => !!p.profile)
+      .map(p => ({
+        'Proposal ID': p.proposalID,
+        'Short Title': p.shortTitle,
+        'Network': p.profile.network,
+        'Assigned TIC/RIC': p.assignToInstitution,
+        'Type': p.profile.type,
+        'Has Linked Studies': p.profile.linkedStudies,
+        'Design': p.profile.design,
+        'Study is Randomized': p.profile.isRandomized,
+        'Randomization Unit': p.profile.randomizationUnit,
+        'Randomization Feature': p.profile.randomizationFeature,
+        'Ascertainment': p.profile.ascertainment,
+        'Observations': p.profile.observations,
+        'Pilot Study': p.profile.isPilot,
+        'Is Registry?': p.profile.isRegistry,
+        'EHR Data Transfer': p.profile.ehrDataTransfer,
+        'EHR Data Transfer Option': p.profile.ehrDatatransferOption,
+        'Study Requires Consent': p.profile.isConsentRequired,
+        'Is EFIC': p.profile.isEfic,
+        'IRB Type': p.profile.irbType,
+        'Regulatory Classification': p.profile.regulatoryClassification,
+        'ClinicalTrials.gov ID': p.profile.clinicalTrialsGovId,
+        'DSMB/DMC Required': p.profile.isDsmbDmcRequired,
+        'Initial ParticipatingSiteCount': p.profile.initialParticipatingSiteCount,
+        'Enrollment Goal': p.profile.enrollmentGoal,
+        'Initial Projected Enrollment Duration': p.profile.initialProjectedEnrollmentDuration,
+        'Lead PIs': p.profile.leadPIs,
+        'Awardee Site Acronym': p.profile.awardeeSiteAcronym,
+        'Primary Funding Type': p.profile.primaryFundingType,
+        'Funded Primarily by Infrastructure': p.profile.isFundedPrimarilyByInfrastructure,
+        'Was Previously Funded': p.profile.isPreviouslyFunded,
+        'Date Funding was Awarded': p.profile.fundingAwardDate,
+        'Phase': p.phase,
 
-    const handleClickOpen = event => {
-        setPopperAnchor(event.currentTarget)
-        setOpen(prev => !prev);
-    }
+        // studies table columns
+        'Short Title': p.shortTitle,
+        'PI': p.piName,
+        'Proposal Status': p.proposalStatus,
+        'Therapeutic Area': p.therapeuticArea,
+        'Submitting Institution': p.submitterInstitution,
+        'Assigned TIC/RIC': p.assignToInstitution,
+        'Submission Date': p.dateSubmitted,
+        'PAT Review Date': p.meetingDate,
+        'Planned Grant Submission Date': p.plannedGrantSubmissionDate,
+        'Actual Grant Submission Date': p.actualGrantSubmissionDate,
+        'Grant Award Date': p.actualGrantAwardDate,
+        'Funding Amount': p.fundingAmount,
+        'Funding Period': p.fundingPeriod,
+        
+        // proposals table columns
+        // 'COVID Study': p.covidStudy,
+        // 'fundingStatusWhenApproved': p.fundingStatusWhenApproved,
+        // 'Funding Status': p.fundingStatus,
+        // 'Study Population': p.studyPopulation,
+        // 'Phase': p.phase,
+        // 'Funding Source': p.fundingSource,
+        // 'Funding Insitute 1': p.fundingInstitute,
+        // 'Funding Insitute 2': p.fundingInstitute2,
+        // 'Funding Insitute 3': p.fundingInstitute3,
+        // 'New Funding Source': p.newFundingSource,
+        // 'Funding Source Confirmation': p.fundingSourceConfirmation,
+        // 'Actual Funding Start Date': p.actualFundingStartDate,
+        // 'Estimated Funding Start Date': p.estimatedFundingStartDate,
+        // 'Actual Protocol Final Date': p.actualProtocolFinalDate,
+        // 'Approval Release Diff': p.approvalReleaseDiff,
+        // 'Initial Contact Date': p.firstContact,
+        // 'Kick-off Meeting Date': p.kickOff,
+        // 'Number of CTSA Program Hub Sites': p.numberCTSAprogHubSites,
+        // 'Number of Sites': p.numberSites,
+        // 'Recommend for Comprehensive Consultation': p.approvedForComprehensiveConsultation,
+      }))
+  }, [proposals])
 
-    return (
-        <div>
-            <Tooltip title="Download study reports" aria-label="Download study reports">
+  return (
+    <Fragment>
+      <Tooltip title="Download study reports" aria-label="Download study reports">
+        <Button
+          variant="outlined"
+          data={ reports }
+          separator=","
+          filename="study-reports"
+          startIcon={ <DownloadIcon /> }
+          onClick={ handleClickOpen }
+        >Study Reports</Button>
+      </Tooltip>
+      <Popper
+        open={ open }
+        anchorEl={ popperAnchor }
+        placement="bottom"
+        transition
+        style={{ zIndex: 99 }}
+      >
+        {
+          ({ TransitionProps }) => (
+            <Fade { ...TransitionProps } timeout={ 350 }>
+              <Paper style={{
+                width: '200px',
+                display: 'flex',
+                flexDirection: 'column',
+              }}>
+                <Typography variant="h6" style={{ padding: '0.5rem 1rem' }}>Select Export Fields</Typography>
+                <Divider />
+                <FormGroup style={{ padding: '0.5rem 1rem' }}>
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        checked={ exportFields.studies }
+                        onChange={ handleToggleExportFields('studies') }
+                      />
+                    }
+                    label="Studies Fields"
+                  />
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        checked={ exportFields.proposals }
+                        onChange={ handleToggleExportFields('proposals') }
+                      />
+                    }
+                    label="Proposals Fields"
+                  />
+                </FormGroup>
+                <Divider />
                 <Button
-                    variant="outlined"
-                    data={ reports }
-                    separator=","
-                    filename="study-reports"
-                    startIcon={ <DownloadIcon /> }
-                    onClick={ handleClickOpen }
-                >Study Reports</Button>
-            </Tooltip>
-            <Popper
-                open={ open }
-                anchorEl={ popperAnchor }
-                placement="bottom"
-                transition
-            >
-                {
-                    ({ TransitionProps }) => (
-                        <Fade { ...TransitionProps } timeout={ 350 }>
-                            <Paper>
-                                <input
-                                    type="checkbox"
-                                    data-field="studies"
-                                    checked={ exportFields.studies }
-                                    onChange={ handleToggleExportFields }
-                                />
-                                <input
-                                    type="checkbox"
-                                    data-field="proposals"
-                                    checked={ exportFields.proposals }
-                                    onChange={ handleToggleExportFields }
-                                />
-                                <IconButton
-                                    component={ CSVLink }
-                                    data={ reports }
-                                    separator=","
-                                    filename="study-reports"
-                                ><DownloadIcon /></IconButton>
-                            </Paper>
-                        </Fade>
-                    )
-                }
-            </Popper>
-        </div>
-    )
+                  component={ CSVLink }
+                  data={ reports }
+                  separator=","
+                  filename="study-reports"
+                >Download</Button>
+              </Paper>
+            </Fade>
+          )
+        }
+      </Popper>
+    </Fragment>
+  )
 }
